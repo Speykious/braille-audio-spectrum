9eb898b363b9674954018cec586c734f1ff052c5
Speykious <speykious@gmail.com>
2020-11-09 04:19:21 +0100

    braille
    Signed-off-by: Speykious <speykious@gmail.com>

--- src/braille.rs
+++ src/braille.rs
@@ -1,23 +1,35 @@
 use std::fs::File;
 use std::path::Path;
 use std::io::{BufRead, BufReader};
+// use anyhow::anyhow;
 
 pub struct BraillePlot {
-  braille: [char; 256],
+  pub braille: [char; 256],
   colgrad: (u32, u32),
   position: (usize, usize),
   dims: (usize, usize),
 }
 
 impl BraillePlot {
-  fn new<P>(path: P, colgrad: (u32, u32), position: (usize, usize), dims: (usize, usize)) -> Result<BraillePlot, anyhow::Error>
+  pub fn new<P>(path: P, colgrad: (u32, u32), position: (usize, usize), dims: (usize, usize)) -> Result<BraillePlot, anyhow::Error>
   where P: AsRef<Path> {
-    let braille = File::open("braille.txt")?;
+    let braille = File::open(path)?;
     let lines = BufReader::new(braille).lines();
     
-    let braille_plot = BraillePlot {
+    let mut braille_plot = BraillePlot {
       braille: [' '; 256],
       colgrad, position, dims,
+    };
+
+    let mut i = 0;
+    for resline in lines {
+      let line = resline?;
+      for c in line.chars() {
+        braille_plot.braille[i] = c;
+        i += 1;
+      }
     }
+    
+    Ok(braille_plot)
   }
 }
